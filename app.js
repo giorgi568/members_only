1f6081f29b1f4bddb29b2da725844f8e6471d76e
giorgi568 <mateshvili02@mail.ru>
2024-03-07 23:31:34 +0400

    clean up

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const session = require('express-session');
 const passport = require('passport');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -38,7 +37,6 @@ app.use(session({ secret: secret, resave: false, saveUninitialized: true }));
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
